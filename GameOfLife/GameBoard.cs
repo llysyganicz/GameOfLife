3188e8ecd9607d0759fc2a5df567dea41092ad16
Łukasz Łysyganicz <lysy01@gmail.com>
2017-11-22 23:09:38 +0100

    Game of Life + SadConsole

---
+++ GameOfLife/GameBoard.cs
@@ -0,0 +1,54 @@
+﻿namespace GameOfLife
+{
+    public class GameBoard
+    {
+        private readonly int _width;
+        private readonly int _height;
+        private bool[,] _board;
+        public bool[,] Board => _board;
+        
+        public GameBoard(int width, int height)
+        {
+            _width = width;
+            _height = height;
+            _board = new bool[_width, _height];
+        }
+
+        public void Update()
+        {
+            _board = CheckBoard();
+        }
+
+        private bool[,] CheckBoard()
+        {
+            var newBoard = new bool[_width, _height];
+
+            for (var x = 0; x < _width; x++)
+            {
+                for (var y = 0; y < _height; y++)
+                {
+                    newBoard[x, y] = IsCellAlive(x, y);
+                }
+            }
+            
+            return newBoard;
+        }
+
+        private bool IsCellAlive(int x, int y)
+        {
+            var count = 0;
+            for (var i = -1; i <= 1; i++)
+            {
+                for (var j = -1; j <= 1; j++)
+                {
+                    var ix = x + i;
+                    var iy = y + j;
+                    if((i == 0 && j == 0) || ix < 0 || iy < 0 || ix >= _width || iy >= _height) continue;
+                    if (Board[ix, iy]) ++count;
+                }
+            }
+
+            return Board[x, y] && (count == 2 || count == 3) || Board[x, y] == false && count == 3;
+        }
+    }
+}
---
+++ GameOfLife/Program.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using Microsoft.Xna.Framework;
+
+namespace GameOfLife
+{
+    class Program
+    {
+        private const int Width = 50;
+        private const int Height = 50;
+        private static GameBoard _board;
+        private static SadConsole.Console _console;
+
+        static void Main()
+        {
+            // Setup the engine and creat the main window.
+            SadConsole.Game.Create("C64.font", Width, Height);
+            
+            // Hook the start event so we can add consoles to the system.
+            SadConsole.Game.OnInitialize = Init;
+
+            // Hook the update event that happens each frame so we can trap keys and respond.
+            SadConsole.Game.OnUpdate = Update;
+
+            SadConsole.Game.OnDraw = Draw;
+            
+            // Start the game.
+            SadConsole.Game.Instance.Run();
+
+            //
+            // Code here will not run until the game window closes.
+            //
+            
+            SadConsole.Game.Instance.Dispose();
+        }
+        
+        private static void Update(GameTime time)
+        {
+            // Called each logic update.
+            _board.Update();
+            
+            // As an example, we'll use the F5 key to make the game full screen
+            if (SadConsole.Global.KeyboardState.IsKeyReleased(Microsoft.Xna.Framework.Input.Keys.F5))
+            {
+                SadConsole.Settings.ToggleFullScreen();
+            }
+        }
+
+        private static void Draw(GameTime time)
+        {
+            PrintBoard();
+        }
+
+        private static void Init()
+        {
+            // Any custom loading and prep. We will use a sample console for now
+
+            _console = new SadConsole.Console(Width, Height);
+
+            _board = new GameBoard(Width, Height);
+            var random = new Random();
+            for (var i = 0; i < Width; i++)
+            {
+                for (var j = 0; j < Height; j++)
+                {
+                    _board.Board[i, j] = random.Next(2) == 1;
+                }
+            }
+            PrintBoard();
+            
+            // Set our new console as the thing to render and process
+            SadConsole.Global.CurrentScreen = _console;
+        }
+
+        private static void PrintBoard()
+        {
+            for (var i = 0; i < Width; i++)
+            {
+                for (var j = 0; j < Height; j++)
+                {
+                    _console.Fill(new Rectangle(i, j, 1, 1), null, _board.Board[i, j] ? Color.White : Color.Black, 0);
+                }
+            }
+        }
+    }
+}
